c27f646e49fd07a9858722044fe7d61518c424c6
jikkun <henjuck@gmail.com>
2021-04-06 14:56:13 +0900

    Initial commit

---
+++ src/main/java/com/legostudy/config/SecurityConfig.java
@@ -0,0 +1,20 @@
+package com.legostudy.config;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configurable
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+//        super.configure(http);
+        http.authorizeRequests()
+                .mvcMatchers("/", "/login", "/sign-up", "/check-email", "/check-email-token", "/email-login", "/check-email-login", "/login-link").permitAll()
+                .mvcMatchers(HttpMethod.GET, "/profile/*").permitAll()
+                .anyRequest().authenticated();
+    }
+}
---
+++ src/test/java/com/legostudy/LegostudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.legostudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LegostudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
